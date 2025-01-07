e89924b2fa83d81582f79e117f85fab7f9c0d708
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2025-01-07 15:42:38 +0100

    fix(auth): add missing mapping profile

---
+++ backend/BlogFlow.Auth/BlogFlow.Auth.Application.UseCases/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BlogFlow.Auth.Application.DTO;
+using BlogFlow.Auth.Domain.Entities;
+
+namespace BlogFlow.Auth.Application.UseCases.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+        }
+    }
+}
