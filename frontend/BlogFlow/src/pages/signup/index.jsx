b3f012848cf9a507bd370341e3755fd77902d46b
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2025-01-20 17:52:50 +0100

    feat(web): create signup page layout

--- frontend/BlogFlow/src/pages/signup/index.jsx
+++ frontend/BlogFlow/src/pages/signup/index.jsx
@@ -1,9 +1,36 @@
 import "./style.css";
 
 function Signup() {
+  const doSignup = () => {};
+
   return (
     <section className="signup">
-        Signup
+      <div className="signup-container">
+        <h2>Sign Up</h2>
+        <form onSubmit={doSignup} className="signup-form">
+          <div className="sigup-several-fields-inline-section">
+            <div className="signup-firstname-section">
+              <label>First name</label>
+              <input placeholder="First Name" />
+            </div>
+            <div className="signup-lastname-section">
+              <label>Last name</label>
+              <input placeholder="Last Name" />
+            </div>
+          </div>
+          <div className="signup-one-field-inline-section">
+            <label>Work email</label>
+            <input placeholder="Work email" />
+          </div>
+          <div className="signup-one-field-inline-section">
+            <label>Password</label>
+            <input placeholder="Password" type="password" />
+          </div>
+          <div className="signup-button-container">
+            <button type="submit">Create Account</button>
+          </div>
+        </form>
+      </div>
     </section>
   );
 }
