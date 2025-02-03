88ee1c65af5b5e8b22b9e980ea0bbf8472dc3dfd
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2025-02-03 07:54:53 +0100

    feat(core): update relation between Post and Content

--- backend/BlogFlow.Auth/BlogFlow.Common.Persistence/Migrations/20250203065257_UpdatePostRelationToContent.Designer.cs
+++ backend/BlogFlow.Auth/BlogFlow.Common.Persistence/Migrations/20250203065257_UpdatePostRelationToContent.Designer.cs
@@ -3,6 +3,7 @@ using BlogFlow.Common.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlogFlow.Common.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250203065257_UpdatePostRelationToContent")]
+    partial class UpdatePostRelationToContent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- backend/BlogFlow.Core/BlogFlow.Core.Domain/Entities/Post.cs
+++ backend/BlogFlow.Core/BlogFlow.Core.Domain/Entities/Post.cs
@@ -7,5 +7,7 @@
         public string Description { get; set; }
         public int BlogId { get; set; }
         public Blog Blog { get; set; }
+        public int ContentId { get; set; }
+        public Content Content { get; set; }
     }
 }
