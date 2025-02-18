496553ade2da38ee52428deef501e71412e5afeb
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2025-02-18 18:07:35 +0100

    feat(web): update add button in home page

--- frontend/BlogFlow/src/components/buttons/roundedButton/index.jsx
+++ frontend/BlogFlow/src/components/buttons/roundedButton/index.jsx
@@ -1,12 +1,12 @@
 
-import style from "./style.css"
+import "./style.css"
 
 function RoundedButton(props) {
   return (
     <button
       title={props.title}
       type="button"
-      className={`{style.rounded-button-container ${props.type}`}
+      className={`rounded-button-container ${props.type}`}
       onClick={props.onclick}
     ></button>
   );
