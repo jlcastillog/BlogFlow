4ab44e37764157ca208bca3d1c5316b06f88604a
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2025-02-24 18:34:54 +0100

    feat(core): update Post definition

--- backend/BlogFlow/BlogFlow.Core.Application.DTO/PostDTO.cs
+++ backend/BlogFlow/BlogFlow.Core.Application.DTO/PostDTO.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
+        public string? HtmlContent { get; set; }
+        public int BlogId { get; set; }
     }
 }
