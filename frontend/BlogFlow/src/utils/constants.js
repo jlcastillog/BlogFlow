55aeac619889ac9c35dd7bcabd2517a528114da2
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2025-03-08 11:59:38 +0100

    conf(web): Cconfiguration by environment

--- frontend/BlogFlow/src/utils/constants.js
+++ frontend/BlogFlow/src/utils/constants.js
@@ -1,11 +1,11 @@
-export const URL_API_AUTH_BASE = "http://localhost:5000/api/v2/";
+export const URL_API_AUTH_BASE = import.meta.env.VITE_URL_API_AUTH_BASE;
 export const API_POST_AUTH = "Users/Authenticate";
 export const API_POST_LOGOUT = "Users/Logout";
 export const API_POST_INSERT = "Users/Insert";
 export const API_POST_UPDATE = "Users/update";
 export const API_POST_REFRESH_TOKEN = "Users/refresh";
 
-export const URL_API_CORE_BASE = "http://localhost:5001/api/v1/";
+export const URL_API_CORE_BASE = import.meta.env.VITE_URL_API_CORE_BASE;
 export const API__BLOG_POST_INSERT = "Blogs/Insert";
 export const API__BLOG_POST_GETALL = "Blogs/GetAll";
 export const API__POST_POST_INSERT = "Posts/Insert";
