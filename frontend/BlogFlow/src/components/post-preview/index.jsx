735e178d2ca3821ada2fbf1daa9833d3af8bab5b
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2025-04-06 12:48:22 +0200

    feat(web): Introduce remove post

--- frontend/BlogFlow/src/components/post-preview/index.jsx
+++ frontend/BlogFlow/src/components/post-preview/index.jsx
@@ -1,11 +1,15 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import truncate from "html-truncate";
 import { useAuth } from "../auth";
 import RoundedButton from "../buttons/roundedButton";
+import { deletePost } from "../../services/post/postService";
+import ErrorPanel from "../../components/error";
 import "./style.css";
 
-function PostPreview({ post }) {
+function PostPreview({ post, onRemovedPost }) {
   const navigate = useNavigate();
+  const [error, setError] = useState(null);
   const auth = useAuth();
 
   const loggedUser = auth.user;
@@ -13,24 +17,46 @@ function PostPreview({ post }) {
   const content = truncate(post.htmlContent, 90);
   const onNavigatePost = () => {
     navigate(`/post/${post.id}`, { state: { post } });
-  }
+  };
 
   const onEditPost = (event) => {
-    event.stopPropagation(); 
+    event.stopPropagation();
     navigate(`/editPost/${post.id}`, { state: { post } });
-  }
+  };
+
+  const onRemovePost = async (event) => {
+    event.stopPropagation();
+    try {
+      event.preventDefault();
+      await deletePost(post.id);
+      onRemovedPost(post);
+    } catch (err) {
+      setError(err.message);
+      if (err.message === "Refresh token failed") {
+        auth.resetUser();
+      }
+    }
+  };
 
   return (
     <section className="post-preview" onClick={onNavigatePost}>
-      {loggedUser && (<div className="edit-button-post-preview">
+      {loggedUser && (
+        <div className="buttons-post-preview">
+          <div className="edit-button-post-preview">
+            <RoundedButton title="Edit post" type="edit" onclick={onEditPost} />
+          </div>
+          <div className="remove-button-post-preview">
             <RoundedButton
-              title="Edit post"
-              type="edit"
-              onclick={onEditPost}
+              title="Remove post"
+              type="remove"
+              onclick={onRemovePost}
             />
-          </div>)}
+          </div>
+        </div>
+      )}
       <h2>{post.title}</h2>
       <div dangerouslySetInnerHTML={{ __html: content }} />
+      <ErrorPanel message={error} />
     </section>
   );
 }
