1dc465a3d7d8e1f5493287692159b1819e652595
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2025-05-09 17:12:28 +0200

    feat(core): add image storage service for cloudinary

---
+++ backend/BlogFlow/BlogFlow.Core.Application.Interface/Services/IImageStorageService.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogFlow.Core.Application.Interface.Services
+{
+    public interface IImageStorageService
+    {
+        Task<(bool result, string Url, string PublicId)> UploadImageAsync(Stream fileStream, string fileName, CancellationToken cancellationToken = default);
+        Task<(bool result, string Url, string PublicId)> UpdateImageAsync(string PublicId, Stream fileStream, string fileName, CancellationToken cancellationToken = default);
+        Task<bool> RemoveImageAsync(string PublicId, CancellationToken cancellationToken = default);
+    }
+}
