bd34cd141c23bb9c53677a8b556c939a5789dc03
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2025-05-12 17:07:22 +0200

    feat(web): adapt frontend to point to API Gateway

--- frontend/BlogFlow/src/utils/constants.js
+++ frontend/BlogFlow/src/utils/constants.js
@@ -1,11 +1,11 @@
-export const URL_API_AUTH_BASE = import.meta.env.VITE_URL_API_AUTH_BASE;
+export const URL_API_AUTH_BASE = import.meta.env.VITE_URL_BASE + import.meta.env.VITE_URL_API_AUTH_VERSION;
 export const API_POST_AUTH = "Users/Authenticate";
 export const API_POST_LOGOUT = "Users/Logout";
 export const API_POST_INSERT = "Users/Insert";
 export const API_POST_UPDATE = "Users/update";
 export const API_POST_REFRESH_TOKEN = "Users/refresh";
 
-export const URL_API_CORE_BASE = import.meta.env.VITE_URL_API_CORE_BASE;
+export const URL_API_CORE_BASE = import.meta.env.VITE_URL_BASE + import.meta.env.VITE_URL_API_CORE_VERSION;
 export const API__BLOG_POST_INSERT = "Blogs/Insert";
 export const API__BLOG_POST_GETALL = "Blogs/GetAll";
 export const API__BLOG_POST_UPDATE = "Blogs/Update";
