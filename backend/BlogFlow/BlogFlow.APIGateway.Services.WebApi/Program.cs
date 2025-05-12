25ecf5aaf423561a9b350297e3d5f395ad25f3d4
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2025-05-12 18:02:06 +0200

    conf: add configuration for docker-dev enviroment

--- backend/BlogFlow/BlogFlow.APIGateway.Services.WebApi/Program.cs
+++ backend/BlogFlow/BlogFlow.APIGateway.Services.WebApi/Program.cs
@@ -2,6 +2,11 @@ using BlogFlow.APIGateway.Services.WebApi.Modules.Features;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Detect current environment
+var environment = builder.Environment.EnvironmentName;
+
+Console.WriteLine($"Running in: {environment}");
+
 builder.Services.AddFeature(builder.Configuration);
 
 var app = builder.Build();
