94970c0ee68f4454d99d8c442ffd864a6f680879
jlcastillog <98542306+jlcastillog@users.noreply.github.com>
2025-07-04 18:36:35 +0200

    feat(web): add validation on signup page

--- frontend/BlogFlow/src/pages/signup/index.jsx
+++ frontend/BlogFlow/src/pages/signup/index.jsx
@@ -1,12 +1,31 @@
 import { createUser } from "../../services/user/userService";
 import { useAuth } from "../../components/auth";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import "./style.css";
 
 function Signup() {
+  const schema = yup.object().shape({
+    firstName: yup.string().required("First name field is mandatory"),
+    lastName: yup.string().required("Last name field is mandatory"),
+    email: yup.string().email().required("Email field is mandatory"),
+    userName: yup.string().required("User name field is mandatory"),
+    password: yup.string().required("Password field is mandatory"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
   const auth = useAuth();
 
-  const doSignup = async (event) => {
-    event.preventDefault();
+  const doSignup = async () => {
+
     const form = event.target;
     const user = {
       firstName: form[0].value,
@@ -20,8 +39,7 @@ function Signup() {
     try {
       await createUser(user);
       auth.login(user.userName, user.password);
-    }
-    catch (error) {
+    } catch (error) {
       console.error(error);
     }
   };
@@ -30,28 +48,33 @@ function Signup() {
     <section className="signup">
       <div className="signup-container">
         <h2>Sign Up</h2>
-        <form onSubmit={doSignup} className="signup-form">
+        <form onSubmit={handleSubmit(doSignup)} className="signup-form">
           <div className="sigup-several-fields-inline-section">
             <div className="signup-firstname-section">
               <label>First name</label>
-              <input placeholder="First Name" />
+              <input placeholder="First Name" {...register("firstName")} />
+              <p style={styles.error}>{errors.firstName?.message}</p>
             </div>
             <div className="signup-lastname-section">
               <label>Last name</label>
-              <input placeholder="Last Name" />
+              <input placeholder="Last Name" {...register("lastName")}/>
+              <p style={styles.error}>{errors.lastName?.message}</p>
             </div>
           </div>
           <div className="signup-one-field-inline-section">
             <label>User name</label>
-            <input placeholder="User Name" />
+            <input placeholder="User Name" {...register("userName")}/>
+            <p style={styles.error}>{errors.userName?.message}</p>
           </div>
           <div className="signup-one-field-inline-section">
             <label>Email</label>
-            <input placeholder="Email" />
+            <input placeholder="Email" {...register("email")}/>
+            <p style={styles.error}>{errors.email?.message}</p>
           </div>
           <div className="signup-one-field-inline-section">
             <label>Password</label>
-            <input placeholder="Password" type="password" />
+            <input placeholder="Password" type="password" {...register("password")}/>
+            <p style={styles.error}>{errors.password?.message}</p>
           </div>
           <div className="signup-button-container">
             <button type="submit">Create Account</button>
@@ -63,3 +86,11 @@ function Signup() {
 }
 
 export default Signup;
+
+const styles = {
+  error: {
+    color: 'red',
+    margin: 0,
+    fontSize: '0.9em',
+  },
+};
